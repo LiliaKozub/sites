05c00e451f513ba672e153347eb99d15193699d5
LiliaKozub <127996179+LiliaKozub@users.noreply.github.com>
2024-10-13 20:58:42 +0300

    Delete JavaDoc directory

--- JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"InputHandler"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"NPC"},{"p":"org.example","l":"NPCManager"},{"p":"org.example","l":"NPCManagerTest"}];updateSearchResults();
